de6b5a74921afedff1d755161c17225a3e236f0d
Taha Shabirr <163581945+Mohammad-Taha925@users.noreply.github.com>
2024-06-25 17:07:37 +0500

    Update enqueue-scripts-styles.php

--- enqueue-scripts-styles.php
+++ enqueue-scripts-styles.php
@@ -1,1 +1,12 @@
+<?php
+/**
+ * Enqueue scripts and styles.
+ */
+function custom_theme_scripts() {
+    // Enqueue main stylesheet
+    wp_enqueue_style('main-stylesheet', get_stylesheet_uri());
 
+    // Enqueue main JavaScript file
+    wp_enqueue_script('main-javascript', get_template_directory_uri() . '/js/main.js', array('jquery'), null, true);
+}
+add_action('wp_enqueue_scripts', 'custom_theme_scripts');
